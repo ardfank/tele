a07691d18524aea45e40aac1c8b341f264c0fc76
ardfank <hitoriki.batosai@gmail.com>
2023-04-05 05:47:11 +0800

    Nab 05-04-2023 05.46.48

--- ext/vow-firefox/main.js
+++ ext/vow-firefox/main.js
@@ -1,10 +1,14 @@
 VowPrev = function(word){
-  var query = word.linkUrl;
-  chrome.tabs.create({url: "https://gps.networkreverse.com/mov/info.php?url=" + query,active: false});
+	if(word.selectionText!==undefined){
+		chrome.tabs.create({url: "https://gps.networkreverse.com/mov/mov2.php?u="+encodeURIComponent(word.selectionText),active:false});
+	}else{	
+	  var query=(word.linkUrl!==undefined)?word.linkUrl:word.srcUrl;
+	  chrome.tabs.create({url: "https://gps.networkreverse.com/mov/info.php?url=" + encodeURIComponent(query),active: false});
+	}
 };
 
 chrome.contextMenus.create({
   title: "VOW Preview",
-  contexts:["link"],
+  contexts:["link","video","selection"],
   onclick: VowPrev
 });
